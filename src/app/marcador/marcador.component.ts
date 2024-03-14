673a80651b0981f618131648b79de8516937c8eb
Rafael Andres Palmezano Diaz <rafaelandrespalmezanodias@gmail.com>
2024-03-14 08:51:35 -0500

    Commit

--- src/app/marcador/marcador.component.ts
+++ src/app/marcador/marcador.component.ts
@@ -9,6 +9,17 @@ import { EquipoComponent } from './../equipo/equipo.component'
   styleUrl: './marcador.component.css'
 })
 export class MarcadorComponent {
-  
+  equipoLocal: string = 'Equipo L';
+  equipoVisitante: string = 'Equipo V';
+  tiempoSeleccionado: number = 1;
+
+  onTiempoCambiado(tiempo: number): void {
+    this.tiempoSeleccionado = tiempo;
+  }
+
+  onNombresEquiposCambiados(nombres: { equipoLocal: string, equipoVisitante: string }): void {
+    this.equipoLocal = nombres.equipoLocal;
+    this.equipoVisitante = nombres.equipoVisitante;
+  }
 
 }
